d81fae23f8763b24927e1e60b0e87f03b4b3b055
fitsum legesse <fitsumlegesse@icloud.com>
2021-05-01 00:47:53 -0700

    sets up redux and other packages, creates a reducer and action and creates a custom error handler

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import connectDB from '../backend/config/db.js'
 // const products = require('./data/products')
 import colors from 'colors'
-
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 
@@ -12,11 +12,23 @@ dotenv.config()
 connectDB()
 const app = express()
 
+// app.use((req,res,next)=>{
+//     console.log('Hello')
+//     console.log(req.originalUrl)
+//     next()
+// })
+
+
+
+
+
 app.get('/', (req,res)=>{
     res.send('API is running..33.')
 })
 
 app.use('/api/products', productRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 
 
