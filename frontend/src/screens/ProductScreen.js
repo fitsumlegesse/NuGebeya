ec59af7872324449921725f59b7fa0559ba0bc4b
fitsum legesse <fitsumlegesse@icloud.com>
2021-05-02 19:54:42 -0700

    creates a mechanism to grab product detail from the db and passes thru redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,27 +1,30 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector} from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import ButtonM from '@material-ui/core/Button'
-import axios from 'axios'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import { listProductDetails } from '../actions/productActions'
 
 
   
 
 
 const ProductScreen = ({ match }) => {
-    const [product, setProduct] = useState({})
+    const dispatch = useDispatch()
 
+    const productDetails = useSelector( state => state.productDetails)
+    const { loading, error, product } = productDetails
     useEffect(() =>{
-        const fetchProduct = async () =>{
-            const { data } = await axios.get(`/api/products/${match.params.id}`)
-            setProduct(data)
-        }
- 
-        fetchProduct()
-    }, [match])
-    console.log("Hellllllllo Products")
+        dispatch(listProductDetails(match.params.id))
+    }, [dispatch, match])
+
+    
+
+    
     return (
         <>
         <Link style={{ textDecoration: 'none' }} to='/'>
@@ -35,68 +38,69 @@ const ProductScreen = ({ match }) => {
                     Go Back
             </ButtonM>
         </Link>
+        {loading ? <Loader /> : error ? <Message severity='error'>{error}</Message>: (
+
+<Row>
+<Col md={6}>
+    <Image src={product.image} alt={product.name} fluid />
+</Col>
+<Col md={3}>
+    <ListGroup variant='flush'>
+        <ListGroup.Item>
+            <h3>{product.name}</h3>
+        </ListGroup.Item>
+        <ListGroup.Item>
+           <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        </ListGroup.Item>
+        <ListGroup.Item>
+            Price: ${product.price}
+        </ListGroup.Item>
+        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+    </ListGroup>
+</Col>
+<Col md={3}>
+    <Card>
+        <ListGroup variant='flush'>
+            <ListGroup.Item>
+                <Row>
+                    <Col>
+                        Price:
+                    </Col>
+                    <Col>
+                        <strong>${product.price}</strong>
+                    </Col>
+                </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+                <Row>
+                    <Col>
+                        Status:
+                    </Col>
+                    <Col>
+                        {product.countInStock > 0? 'In Stock' : 'Out of Stock'}
+                    </Col>
+                </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+                <ButtonM
+                 endIcon={<AddShoppingCartIcon />}
+                 className="Button"
+                 size="large"
+                 variant="contained"
+                 color="primary"
+                 style={{background:'#7699d4', }}
+                >
+                    Add To Cart
+                </ButtonM>
+                
+            </ListGroup.Item>
+        </ListGroup>
+    </Card>
+</Col>
+</Row>
+        )}
             
-            {/* <Link className='btn btn-dark my-3' to='/'>
-                Go Back
-            </Link> */}
-            <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid />
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                           <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Price: ${product.price}
-                        </ListGroup.Item>
-                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        Price:
-                                    </Col>
-                                    <Col>
-                                        <strong>${product.price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        Status:
-                                    </Col>
-                                    <Col>
-                                        {product.countInStock > 0? 'In Stock' : 'Out of Stock'}
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <ButtonM
-                                 endIcon={<AddShoppingCartIcon />}
-                                 className="Button"
-                                 size="large"
-                                 variant="contained"
-                                 color="primary"
-                                 style={{background:'#7699d4', }}
-                                >
-                                    Add To Cart
-                                </ButtonM>
-                                
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+            
         </>
     )
 }
