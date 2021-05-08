88310f2ff688e6fc17432e38222ea165883dd184
fitsum legesse <fitsumlegesse@icloud.com>
2021-05-08 00:56:37 -0700

    adds a middleware that protects private routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,28 @@ const authUser = asyncHandler(async(req, res) => {
     }
 })
 
+// Get user profile
+//GET/api/users/profile
+// Private 
+
+const getUserProfile  = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+    console.log(user)
+
+    if(user){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    }else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+    }
+)
 
 
-export {authUser}
+
+export {authUser, getUserProfile}
