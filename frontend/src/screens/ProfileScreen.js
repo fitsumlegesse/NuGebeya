194b1208bc4c184e200ac7e19b3bd3a821ef6b62
fitsum legesse <fitsumlegesse@icloud.com>
2021-05-13 23:29:33 -0700

    adds a mechanism to update user profile

---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col} from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader  from '../components/Loader'
+import { getUserDetails } from '../actions/userAction'
+// import { userDetailsReducer } from '../reducers/userReducers'
+
+const ProfileScreen = ({ location, history }) => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState(null)
+
+    const userDetails = useSelector(state => state.userDetails)
+    const { loading, error, user  } = userDetails
+    
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(!userInfo){
+            history.push('/login')
+        } else {
+            if(!user.name){
+                dispatch(getUserDetails('profile'))
+            }else{
+                console.log("object")
+                setName(user.name)
+                setEmail(user.email)
+            }
+        }
+    }, [dispatch, history, userInfo, user])
+
+
+    const submitHandler = (e) =>{
+        e.preventDefault()
+        if(password !== confirmPassword)
+        {
+            setMessage('Password do not match')
+            console.log("pwd do not match ")
+        }else{
+            //Dispatch
+        }
+        
+
+    }
+
+    return (
+        <Row>
+            <Col md={3}>
+            <h2>User Profile</h2>
+           {message && <Message varient='danger'>{message}</Message>}
+           {error && <Message varient='danger'>{error}</Message>}
+           {loading && <Loader />}
+
+        <Form onSubmit={submitHandler}>
+
+           <Form.Group controlId='name'>
+                <Form.Label> Name </Form.Label>
+                <Form.Control
+                    type='name'
+                    placeholder='Enter name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            
+            <Form.Group controlId='email'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                    type='email'
+                    placeholder='Enter email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Enter password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='confirmPassword'>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Confirm password'
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <Button type='submit' varient='primary' >
+               Updates
+            </Button>
+        </Form>
+
+
+            </Col>
+            <Col md={9}>
+                <h2>Mr Orders</h2>
+            </Col>
+        </Row>
+    )
+}
+
+export default ProfileScreen
