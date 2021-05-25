f42abb6e9c7ac66746503225d0f6391bd965b128
fitsum legesse <fitsumlegesse@icloud.com>
2021-05-24 21:55:29 -0700

    creates a mechanism that allows order to be retrived by order id and creates constant, action and reducer to display order info on the screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -29,4 +29,25 @@ const addOrderItems = asyncHandler(async(req, res) => {
     }
 })
 
-export {addOrderItems}
+
+// @desc get order by ID
+// @route POST /api/orders/:id
+// @access Private 
+
+const getOrderById = asyncHandler(async(req, res) => {
+    const order =  await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    )
+
+    if(order)
+    {
+        res.json(order)
+
+    }else{
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export {addOrderItems, getOrderById}
